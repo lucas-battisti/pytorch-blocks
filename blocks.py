11798c7eb9978f0b63d032f9dde3fd7767207212
Lucas Battisti <lucas.sala.battisti.pessoal@gmail.com>
2024-04-03 07:23:35 -0300

    conv2

--- blocks.py
+++ blocks.py
@@ -1,7 +1,7 @@
 from math import floor
 import torch
 from torch import nn
-from typing import Callable, List, Union
+from typing import Callable, List, Union, Tuple
 
 
 class LinearBlock(nn.Module):
@@ -159,6 +159,106 @@ class Conv1dBlock(nn.Module):
 
     def forward(self, input):
         return self.sequential(input)
+    
+
+class Conv2dBlock(nn.Module):
+    """
+    A custom neural network module consisting of:
+    - Two dimensional convulational layer ``nn.Conv1d``;
+    - Normalization layer (as Batch Normalization or Layer Normalization);
+    - Nonlinear activation function;
+    - Dropout layer.
+    - Pooling layer (maxpool or averagepool).
+
+    Args:
+        in_features (int): Size of each input sample.
+        out_features (int): Size of each output sample.
+        bias (bool): If set to ``False``, the linear layer will not learn an additive bias.
+            Defaults to ``True``.
+        norm_layer (Callable[..., torch.nn.Module]): Normalization layer.
+            If set ``None`` this layer won't be used (Default).
+        activation_function (Callable[..., torch.nn.Module]): Nonlinear activation function.
+            If set ``None`` will use identity function (Default).
+        dropout_prob (float): The probability for the dropout layer. Defaults to 0.0.
+        pooling_layer (Callable[..., torch.nn.Module]): Pooling layer.
+            If set ``None`` this layer won't be used (Default).
+        layer_order (Union[str, List[str]]): Representation of the layer's ordering.
+            Defaults to "cnadp".
+    """
+    def __init__(self, *args, in_channels: int, out_channels: int, kernel_size: Union[int, Tuple[int, int]], bias: bool = True,
+                 stride: Union[int, Tuple[int, int]] = 1, padding: Union[int, Tuple[int, int]] = 0, dilation: Union[int, Tuple[int, int]] = 1,
+                 norm_layer: Callable[..., nn.Module] = None, norm_layer_args: dict = {},
+                 activation_function: Callable[..., nn.Module] = None, activation_function_args: dict = {},
+                 dropout_prob: float = 0.0,
+                 pooling_layer: Callable[..., nn.Module] = None, pooling_layer_args: dict = {},
+                 layers_order: Union[str, List[str]] = "cnadp",
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.layers_dict = {"c": None, "n": None, "a": None, "d": None, "p": None}
+
+        self.layers_dict["c"] = nn.Conv2d(
+            in_channels=in_channels, out_channels=out_channels,
+            kernel_size=kernel_size, bias=bias,
+            stride=stride, padding=padding, dilation=dilation)
+
+        if norm_layer is not None:
+            self.layers_dict["n"] = norm_layer(**norm_layer_args)
+
+        if activation_function is not None:
+            self.layers_dict["a"] = activation_function(**activation_function_args)
+
+        if dropout_prob != 0.0:
+            self.layers_dict["d"] = nn.Dropout2d(dropout_prob)
+
+        if pooling_layer is not None:
+            self.layers_dict["p"] = pooling_layer(**pooling_layer_args)
+            
+            self.pool_kernel_size = self.layers_dict["p"].kernel_size
+            if isinstance(self.pool_kernel_size, tuple):
+                self.pool_kernel_size = self.pool_kernel_size[0]
+                
+            self.pool_stride = self.layers_dict["p"].stride
+            if isinstance(self.pool_stride, tuple):
+                self.pool_stride = self.pool_stride[0]
+                
+            try:
+                self.pool_padding = self.layers_dict["p"].padding
+                if isinstance(self.pool_padding, tuple):
+                    self.pool_padding = self.pool_padding[0]
+            except AttributeError:
+                self.pool_padding = 0
+                
+            try:
+                self.pool_dilation = self.layers_dict["p"].dilation
+                if isinstance(self.pool_dilation, tuple):
+                    self.pool_dilation = self.pool_dilation[0]
+            except AttributeError:
+                self.pool_dilation = 1
+
+        self.out_channels = out_channels
+
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+
+        self.sequential = nn.Sequential()
+        for layer in layers_order:
+            if self.layers_dict[layer] is not None:
+                self.sequential = self.sequential.append(self.layers_dict[layer])
+
+    def output_shape(self, L_in):
+        L_out = floor((L_in + 2 * self.padding - self.dilation * (self.kernel_size - 1) - 1) / self.stride + 1)
+        if self.layers_dict["p"] is not None:
+            L_out = floor((L_out + 2 * self.pool_padding - self.pool_dilation * (
+                    self.pool_kernel_size - 1) - 1) / self.pool_stride + 1)
+
+        return {"C_out": self.out_channels, "L_out": L_out}
+
+    def forward(self, input):
+        return self.sequential(input)
+
 
 
 class Conv2dBlock(nn.Module):
